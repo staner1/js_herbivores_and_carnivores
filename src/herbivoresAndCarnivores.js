5e1b7b977e8a5a95518d4f67ca1b891d12537916
Oleksandr <kozyrev.sania@gmail.com>
2025-07-01 19:54:16 +0300

    fixed bugs

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,59 +2,32 @@
 
 class Animal {
   static alive = [];
-  #type = 'none';
 
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
     Animal.alive.push(this);
   }
-
-  setType(type) {
-    this.#type = type;
-  }
-
-  get getType() {
-    return this.#type;
-  }
 }
 
 class Herbivore extends Animal {
   constructor(name) {
     super(name);
-
     this.hidden = false;
-    this.setType('herbivore');
   }
 
   hide() {
-    if (this.hidden === false) {
-      this.hidden = true;
-    } else {
-      this.hidden = false;
-    }
+    this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-
-    this.setType('carnivore');
-  }
-
   bite(object) {
-    if (object.getType !== 'carnivore' && object.hidden !== true) {
+    if (object instanceof Herbivore && object.hidden !== true) {
       object.health -= 50;
     }
 
-    if (object.health <= 0) {
-      const indexItem = Animal.alive.indexOf(object);
-
-      if (indexItem !== -1) {
-        Animal.alive.splice(indexItem, 1);
-      }
-    }
+    Animal.alive = Animal.alive.filter((item) => item.health > 0);
   }
 }
 
