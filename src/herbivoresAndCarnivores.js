defd6a5875863227f178830dcc86e92c3793f045
Oleksandr <kozyrev.sania@gmail.com>
2025-07-01 19:22:31 +0300

    add solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,61 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  #type = 'none';
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  setType(type) {
+    this.#type = type;
+  }
+
+  get getType() {
+    return this.#type;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+    this.setType('herbivore');
+  }
+
+  hide() {
+    if (this.hidden === false) {
+      this.hidden = true;
+    } else {
+      this.hidden = false;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.setType('carnivore');
+  }
+
+  bite(object) {
+    if (object.getType !== 'carnivore' && object.hidden !== true) {
+      object.health -= 50;
+    }
+
+    if (object.health <= 0) {
+      const indexItem = Animal.alive.indexOf(object);
+
+      if (indexItem !== -1) {
+        Animal.alive.splice(indexItem, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
